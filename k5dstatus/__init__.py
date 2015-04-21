e8f2a1f2041c20284d77e1b138ef2cd5cd6ed19a
Jamie Bliss <astronouth7303@gmail.com>
2015-04-21 05:48:10 -0400

    Allow passing a config on the command line.
    Use real command line parsing.

--- k5dstatus/__init__.py
+++ k5dstatus/__init__.py
@@ -3,6 +3,7 @@ import sys
 import os
 import yaml
 import asyncio
+import argparse
 from dbus.mainloop.glib import DBusGMainLoop
 from .service import BlockManager
 from .barproto import BarManager, InputParser
@@ -12,22 +13,27 @@ try:
 except ImportError:
     pass
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--config', '-c', default='~/.k5dstatus.conf',
+                   help='Config file to load')
+
 
 def start():
     """
     Orchestrates the start of everything, injects dependencies, sets up event
     loops, and all the other things that need to happen
     """
-    # FIXME: Use actual argument parsing
     # Remap some stuff so we don't corrupt pipes
     i3bar_blocks, sys.stdout = sys.stdout, sys.stderr
     i3bar_input, sys.stdin = sys.stdin, None
+
+    args = parser.parse_args()
 
     DBusGMainLoop(set_as_default=True)
     #asyncio.set_event_loop_policy(gbulb.GLibEventLoopPolicy())
 
     try:
-        with open("{}/.i3-dstatus.conf".format(os.path.expanduser('~'))) as f:
+        with open(os.path.expanduser(args.config)) as f:
             config = yaml.safe_load(f)
     except FileNotFoundError:
         config = {}
@@ -35,11 +41,10 @@ def start():
     manager = BlockManager(config)
 
     blockman = BarManager(i3bar_blocks, manager, config)
-    parser = InputParser(i3bar_input, manager, config)
+    iparse = InputParser(i3bar_input, manager, config)
 
     GLib.idle_add(run_from_config, config, sys.argv[1:])
-    # FIXME: Feed blockman, parser coroutines to event loop
-
+    # FIXME: Feed blockman, iparse coroutines to event loop
 
     main = GLib.MainLoop()
     main.run()
