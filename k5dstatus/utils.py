900c0125e848233a8812f22c02cc2fc1daecec44
Jamie Bliss <astronouth7303@gmail.com>
2015-04-21 06:48:02 -0400

    First stab at block-like dict

--- k5dstatus/utils.py
+++ k5dstatus/utils.py
@@ -2,8 +2,51 @@
 Utility functions for generators.
 """
 import dbus
+import collections.abc
 from .service import DBUS_SERVICE, PATH_PREFIX, BlockManager, Block
 __all__ = 'get_manager', 'get_block', 'get_config', 'make_block'
+
+
+class BlockDict(collections.abc.MutableMapping):
+    """
+    dict-like wrapper around block objects
+    """
+    def __init__(self, obj):
+        super().__init__()
+        self._block = obj
+
+        # Cache
+        self._block_block = self.block(Block.INTERFACE)
+        self._block_prop = self.block(Block.PROPINTERFACE)
+
+    def block(self, interface=Block.INTERFACE):
+        """
+        Get the block object.
+        """
+        return dbus.Interface(self._block, Block.INTERFACE)
+
+    def __getitem__(self, key):
+        return self._block_prop.Get(Block.INTERFACE, key)
+
+    def __setitem__(self, key, value):
+        return self._block_prop.Set(Block.INTERFACE, key, value)
+
+    def __delitem__(self, key):
+        raise TypeError("Can't delete keys on a BlockDict")
+
+    def __iter__(self):
+        for name in self._block_prop.GetAll(Block.INTERFACE).keys():
+            yield name
+
+    def __len__(self):
+        return len(self._block_prop.GetAll(Block.INTERFACE))
+
+    # Optional performance methods
+
+    def update(self, other):
+        self._block_block.Update(other)
+
+    # TODO: Wrap items() so that it doesn't make repeated d-bus calls for values
 
 
 def get_manager():
@@ -14,10 +57,7 @@ def get_manager():
 
 
 def get_block(bpath):
-    return dbus.Interface(
-        dbus.SessionBus().get_object(DBUS_SERVICE, bpath),
-        Block.INTERFACE
-    )
+    return BlockDict(dbus.SessionBus().get_object(DBUS_SERVICE, bpath))
 
 
 def get_config(app):
